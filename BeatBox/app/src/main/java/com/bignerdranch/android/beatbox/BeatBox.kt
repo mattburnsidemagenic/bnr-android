e2b062cdec2cdaaab416846bef68015461e47c41
Matt Burnside <mburnside@cutera.com>
2021-01-07 09:36:54 -0600

    End of Chapter 20

--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
@@ -1,16 +1,38 @@
 package com.bignerdranch.android.beatbox
 
+import android.content.res.AssetFileDescriptor
 import android.content.res.AssetManager
+import android.media.SoundPool
 import android.util.Log
+import java.io.IOException
 import java.lang.Exception
 
 private const val TAG = "BeatBox"
 private const val SOUNDS_FOLDER = "sample_sounds"
+private const val MAX_SOUNDS = 5
 class BeatBox(private val assets: AssetManager) {
     val sounds: List<Sound>
-
+    private val soundPool = SoundPool.Builder()
+        .setMaxStreams(MAX_SOUNDS)
+        .build()
     init {
         sounds = loadSounds()
+    }
+
+    fun release() {
+        soundPool.release()
+    }
+
+    fun play(sound: Sound) {
+        sound.soundId?.let {
+            soundPool.play(it, 1.0f, 1.0f, 1, 0, 1.0f)
+        }
+    }
+
+    private fun load(sound: Sound) {
+        val afd: AssetFileDescriptor = assets.openFd(sound.assetPath)
+        val soundId = soundPool.load(afd, 1)
+        sound.soundId = soundId
     }
     private fun loadSounds(): List<Sound> {
         val soundNames: Array<String>
@@ -24,6 +46,12 @@ class BeatBox(private val assets: AssetManager) {
         soundNames.forEach {fileName ->
             val assetPath = "$SOUNDS_FOLDER/$fileName"
             val sound = Sound(assetPath)
+            try {
+                load(sound)
+                sounds.add(sound)
+            } catch(ioe: IOException) {
+                Log.e(TAG, "Could not load sound $fileName", ioe)
+            }
             sounds.add(sound)
         }
         return sounds
--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/MainActivity.kt
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/MainActivity.kt
@@ -24,9 +24,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        beatBox.release()
+    }
+
     private inner class SoundHolder(private val binding: ListItemSoundBinding) : RecyclerView.ViewHolder(binding.root) {
         init {
-            binding.viewModel = SoundViewModel()
+            binding.viewModel = SoundViewModel(beatBox)
         }
 
         fun bind(sound: Sound) {
